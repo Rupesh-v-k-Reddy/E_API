644d30260f851750b58e212b69931e5ba61f68ba
Rupesh Reddy <rupeshvkreddy@gmail.com>
2023-07-28 21:03:57 +0530

    added update functionality to update users Email

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,12 @@
 import express from "express"
-import {readAllUsers,readUserById} from "./handlers/users"
+import {readAllUsers,readUserById,updateUserEmail} from "./handlers/users"
 
 
 const router = express.Router();
 
 router.get('/users',readAllUsers);
 router.get("/user/:id",readUserById); //read single user based on userid 
-
+router.post("/user/:id",updateUserEmail) // update user email id
 
 
 export default router
